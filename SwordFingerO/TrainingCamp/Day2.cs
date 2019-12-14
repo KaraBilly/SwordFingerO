b1b8e8ad6c85241a8d6e5bda4544349764001079
Billy <36272368+KaraBilly@users.noreply.github.com>
2019-12-14 23:21:30 +0800

    Update Day2 Solution1

---
+++ SwordFingerO/TrainingCamp/Day2.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SwordFingerO.TrainingCamp
+{
+    public class Day2
+    {
+        public void Rotate(int[] nums, int k)
+        {
+            if (nums == null || nums.Length == 0)
+                return;
+            var len = nums.Length;
+            var rp = k % len;   //rotate point
+            Reserve(nums, 0, len - 1);
+            Reserve(nums, 0, rp-1);
+            Reserve(nums, rp, len - 1);
+        }
+
+        private void Reserve(int[] nums, int start, int end)
+        {
+            while(start < end)
+            {
+                var temp = nums[start];
+                nums[start++] = nums[end];
+                nums[end--] = temp;
+            }
+            //throw new NotImplementedException();
+        }
+    }
+}
